36498b07db27dc412355ca09727c98ce74448ffe
Micael Fiordelmondo <micael.fiordel@gmail.com>
2021-11-25 12:51:40 -0300

    clase 6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navBar/NavBar';
+import ItemListContainer from './containers/ItemListContainer';
 import "tailwindcss/tailwind.css"
 
 function App() {
   return (
     <div className="App">
       <NavBar name="YoYo"/>
+      
     </div>
   );
 }
--- src/components/cartWidget/CartWidget.js
+++ src/components/cartWidget/CartWidget.js
@@ -5,6 +5,7 @@ import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
 import ItemCount from "../itemCount/ItemCount"
+import ItemListContainer from "../../containers/ItemListContainer";
 
 const products = [
   {
@@ -80,6 +81,7 @@ export default function CartWidget() {
                     <div className="mt-8">
                       <div className="flow-root">
                         <ul role="list" className="-my-6 divide-y divide-gray-200">
+                        <ItemListContainer />
                           {products.map((product) => (
                             <li key={product.id} className="py-6 flex">
                               <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
