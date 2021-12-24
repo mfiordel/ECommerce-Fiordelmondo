e7d08a3db709edf73a3b1def973570e147371381
Micael Fiordelmondo <micael.fiordel@gmail.com>
2021-12-24 10:01:23 -0300

    netlify.toml test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/navBar/NavBar';
 import "tailwindcss/tailwind.css"
 import ItemDetailContainer from './containers/ItemDetailContainer';
 import ItemListContainer from './containers/ItemListContainer';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import CartWidget from './components/cartWidget/CartWidget';
 import CartView from './components/cartView/CartView';
 import CartContextProvider from './Context/CartContext';
@@ -13,18 +13,18 @@ import Checkout from './components/Checkout/Checkout';
 function App() {
   return (
     <CartContextProvider >
-    <Router>
-      <NavBar/>
-      <Switch>
-        <Route exact path = "/" ><ItemListContainer/></Route>
-        <Route exact path = "/category/:categoryId"><ItemListContainer/></Route>
-        <Route exact path = "/item/:id" ><ItemDetailContainer/></Route>
-        <Route exact path = "/shop"><CartWidget/></Route>
-        <Route exact path= "/cart"><CartView/></Route>
-        <Route exact path= "/checkout"><Checkout/></Route>
-        <Route element={<h2>Not found</h2>}/>
-      </Switch>
-    </Router>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route exact path = "/" element={<ItemListContainer/>}></Route>
+          <Route exact path = "/category/:categoryId" element={<ItemListContainer/>}></Route>
+          <Route exact path = "/item/:id" element={<ItemDetailContainer/>}></Route>
+          <Route exact path = "/shop" element={<CartWidget/>}></Route>
+          <Route exact path= "/cart" element={<CartView/>}></Route>
+          <Route exact path= "/checkout" element={<Checkout/>}></Route>
+          <Route element={<h2>Not found</h2>}/>
+        </Routes>
+      </Router>
     </CartContextProvider>
   );
 }
